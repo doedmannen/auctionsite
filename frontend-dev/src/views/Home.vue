b2e701b8a8615685e9c408dfd099418d630b98bd
Kate <curran.kate.t@gmail.com>
2019-04-15 15:00:51 +0200

    Bootstrap stuff - working

--- frontend-dev/src/views/Home.vue
+++ frontend-dev/src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
+    <router-view/>
+    <Header/>
     <h1>Welcome to home</h1>
   </div>
 </template>
 
 <script>
+  import Header from "@/components/Header.vue";
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    Header
+  }
 }
 </script>
+
+<style>
+
+</style>

a6252f31be21ee3a580f0679ea6765b016687354
Kate <curran.kate.t@gmail.com>
2019-04-15 15:15:54 +0200

    Mock up header and footer
    mainly for spacing purposes for views

---
+++ frontend-dev/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="fixed-bottom navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Footer</a>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Footer",
+        props: {}
+    };
+</script>
+
+<style>
+
+</style>
--- frontend-dev/src/views/Home.vue
+++ frontend-dev/src/views/Home.vue
@@ -3,20 +3,26 @@
     <router-view/>
     <Header/>
     <h1>Welcome to home</h1>
+    <Footer/>
   </div>
 </template>
 
 <script>
   import Header from "@/components/Header.vue";
+  import Footer from "@/components/Footer.vue";
 
 export default {
   name: 'home',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
+  .home {
+    margin-bottom: 75px;
+  }
 
 </style>

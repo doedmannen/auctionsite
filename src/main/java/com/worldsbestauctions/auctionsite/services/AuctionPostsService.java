c76543421f366fa5b0aecef906626a13ca1d11c0
Anna <zoora1234@hotmail.com>
2019-04-15 14:55:08 +0200

    AuctionPostEntity

--- src/main/java/com/worldsbestauctions/auctionsite/services/AuctionPostsService.java
+++ src/main/java/com/worldsbestauctions/auctionsite/services/AuctionPostsService.java
@@ -1,12 +1,15 @@
 package com.worldsbestauctions.auctionsite.services;
 
+import com.worldsbestauctions.auctionsite.repos.AuctionPostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuctionPostsService {
 
-    
+    @Autowired
+    AuctionPostRepo auctionPostRepo;
+
 
 
 }

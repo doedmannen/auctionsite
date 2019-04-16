eb898c29ceab324d369f8deacabe9e763fbf8cb8
Kristoffer <doedmannen@aetmej.se>
2019-04-16 12:11:10 +0200

    fixed resize and crop so that files no longer get corrupted

--- frontend-dev/src/components/ImageUploader.vue
+++ frontend-dev/src/components/ImageUploader.vue
@@ -32,38 +32,55 @@ export default {
                 let image = new Image();
                 image.onload = () => {
                     let file = this.canvasToFile(image);
-                    this.$store.commit('addUpload', file)
+                    this.$store.commit('addUpload', file._v)
+                    console.log(file._v);
                 }
                 image.src = e.target.result;
             }
         },
-        canvasToFile(image){
+        async canvasToFile(image, width, height){
                 let s, cx, cy, ix, iy, cvs, ctx;
                 cvs = document.createElement("canvas");
                 ctx = cvs.getContext("2d");
-                cx = 800;
-                cy = 600;
+                cx = width || 640;
+                cy = height || 480;
                 cvs.width = cx;
                 cvs.height = cy;
                 // ctx.fillStyle = 'white';
                 // ctx.fillRect(0,0, cx, cy);
                 s = cx / image.width;
                 s = (image.height * s) > cy ? cy / image.height : s;
-                ix = image.width * s;
-                iy = image.height * s;
+                ix = image.width;
+                iy = image.height;
+                if(s < 1){
+                    ix = ix * s;
+                    iy = iy * s;
+                }
+                // Get image into canvas
                 ctx.drawImage(image,
                     (cx / 2 - ix / 2),
                     (cy / 2 - iy / 2), ix, iy);
+                // Watermark
                 ctx.fillStyle = 'black';
                 ctx.strokeStyle = 'white';
                 ctx.font = '26px Arial';
                 ctx.fillText("auctionsite.com", 10, cy-30);
                 ctx.strokeText("auctionsite.com", 10, cy-30);
-                this.pics.push(cvs.toDataURL('image/png'));
-                return new File([cvs.toDataURL('image/png', 0.1)], "file.png", {type: "image/png"});
+                // Convert base64 to byteArray
+                let imgData = cvs.toDataURL('image/png');
+                // Append the resized file to our document
+                this.pics.push(imgData);
+                // return new file
+                return new File([this.dataUrlToFile(imgData)], "file.png", {type: "image/png"});
         },
-        async urlToFile(url, fileName, mimeType){
-            let url = 
+        dataUrlToFile(data){
+            let byteString = atob(data.split(',')[1]);
+            let arrBuffer = new ArrayBuffer(byteString.length);
+            let ui8a = new Uint8Array(arrBuffer)
+            for(let i = 0; i < byteString.length; i++){
+                ui8a[i] = byteString.charCodeAt(i);
+            }
+            return ui8a;
         }
     },
     data(){

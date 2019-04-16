614159436092e6b3ad0904aa6fa2e7f47273e341
Kate <curran.kate.t@gmail.com>
2019-04-16 13:02:38 +0200

    Moved header and footer to App

--- frontend-dev/src/App.vue
+++ frontend-dev/src/App.vue
@@ -1,8 +1,23 @@
 <template>
     <div id="app">
+        <Header/>
         <router-view/>
+        <Footer/>
     </div>
 </template>
+
+<script>
+    import Header from "@/components/Header.vue";
+    import Footer from "@/components/Footer.vue";
+
+    export default {
+        name: "app",
+        components: {
+            Header,
+            Footer
+        }
+    };
+</script>
 
 <style>
     #app {

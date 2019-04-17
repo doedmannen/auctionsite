497b74973f0120a28bf72b900529cec5470e10da
Kate <curran.kate.t@gmail.com>
2019-04-17 11:02:15 +0200

    Dropdowns for registration and login

--- frontend-dev/src/components/Header.vue
+++ frontend-dev/src/components/Header.vue
@@ -8,6 +8,7 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
+
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -22,9 +23,83 @@
                         </div>
                     </div>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/create">Create auction</a>
+                </li>
             </ul>
-            <a href=""><i class="fas fa-user"></i></a>
-            <a href=""><i class="fas fa-envelope"></i></a>
+
+            <!--Registration dropdown-->
+            <li class="dropleft">
+                <a data-toggle="dropdown" class="spacing">Register</a>
+                <ul class="dropdown-menu" role="menu">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p>Create an account</p></div>
+                        <form role="form" autocomplete="off">
+                            <div class="form-group">
+                                <input type="text" name="username" tabindex="1"
+                                       class="form-control" placeholder="Username" value="">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" name="email" tabindex="1" class="form-control"
+                                       placeholder="Email Address" value="">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" tabindex="2"
+                                       class="form-control" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="confirm-password" id="confirm-password"
+                                       tabindex="2" class="form-control" placeholder="Confirm Password">
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-xs-6 col-xs-offset-3">
+                                        <input type="submit"id="register-submit" class="btn btn-dark btn-center" value="Register Now">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </ul>
+            </li>
+
+            <!--Login dropdown-->
+            <li class="dropleft">
+                <a data-toggle="dropdown"><i class="fas fa-user spacing"></i></a>
+                <ul class="dropdown-menu dropdown-menu-lg-left" role="menu">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p>Log In</p></div>
+                        <form role="form" autocomplete="off">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" name="username" id="username" tabindex="1"
+                                       class="form-control" placeholder="Username" value="" autocomplete="off">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password" tabindex="2"
+                                       class="form-control" placeholder="Password" autocomplete="off">
+                            </div>
+
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-xs-5">
+                                        <input type="submit" id="login-submit" class="btn btn-dark btn-center" value="Log In">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </ul>
+            </li>
+
+
+            <a href=""><i class="fas fa-envelope spacing"></i></a>
+            </ul>
         </div>
     </nav>
 </template>
@@ -83,7 +158,21 @@
 
     .fas {
         font-size: 1.75em;
-        margin-right: 1.5em;
     }
 
+    .spacing {
+        margin: 0 1em;
+    }
+
+    li {
+        list-style: none;
+    }
+
+    .dropdown-menu {
+        width: 300px;
+    }
+
+    .btn-center {
+        margin-left: 1em;
+    }
 </style>

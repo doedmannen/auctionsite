49af35af0883043f9d6074c936bffb1fef5c4496
Anna <zoora1234@hotmail.com>
2019-04-23 11:50:24 +0200

    populated categories in navbar

--- frontend-dev/src/components/Header.vue
+++ frontend-dev/src/components/Header.vue
@@ -19,7 +19,7 @@
                         <a class="dropdown-item" href="#">Completed</a>
                         <div class="dropdown-divider"></div>
                         <div v-for="(category, index) in categories" :key="index">
-                            <a :href="category.url" class="dropdown-item">{{ category.name }}</a>
+                            <a :href="'/category/' + category.categoryname" class="dropdown-item">{{ category.categoryname }}</a>
                         </div>
                     </div>
                 </li>
@@ -103,7 +103,7 @@
         data() {
             return {
                 searchAuctions: "",
-                categories: [
+               /* categories: [
                     {
                         name: "Vehicles",
                         url: "/vehicles"
@@ -116,8 +116,13 @@
                         name: "Sporting Goods",
                         url: "/health"
                         //etc etc more categories from db
-                    }]
+                    }]*/
             }
+        },
+         computed: {
+        categories() {
+            return this.$store.state.categories;
+         }
         },
         methods: {
             async createNewUser(){

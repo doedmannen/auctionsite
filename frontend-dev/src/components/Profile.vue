3d8af1404ef133fd6529f97c8282ff780110f098
Kate <curran.kate.t@gmail.com>
2019-04-25 14:20:25 +0200

    Profile page fixes

--- frontend-dev/src/components/Profile.vue
+++ frontend-dev/src/components/Profile.vue
@@ -5,38 +5,46 @@
             <div class="flexSection" id="namePic">
                 <img src="http://www.atabjkpg.se/wp-content/uploads/2017/12/default-user-image.png" id="profileIcon">
                 <h3>{{indivProfile.users.firstname}} {{indivProfile.users.lastname}}</h3>
-                <div v-if="this.me != null">
+                <div v-if="this.me = null">
                     <div v-if="indivProfile.users.userid = this.me.userid">
-                    <button type="button" class="btn btnStyle" data-toggle="modal" data-target="#exampleModal"> Edit details</button>
+                        <button type="button" class="btn btnStyle" data-toggle="modal" data-target="#exampleModal"> Edit
+                            details
+                        </button>
 
-                    <!-- Change details modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">My details</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Save changes"></button>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <h5>Change icon</h5>
-                                    <i class="far fa-smile"></i>
-                                    <i class="fas fa-cat"></i>
-                                    <i class="fas fa-dragon"></i>
-                                    <i class="fas fa-hippo"></i>
-                                    <hr>
-                                    <h5>Change color</h5>
-                                    <span class="color"></span>
-                                    <span class="color"></span>
-                                    <span class="color"></span>
-                                    <span class="color"></span>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btnStyle" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btnStyle">Save changes</button>
+                        <!-- Change details modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">My details</h5>
+                                        <button type="button" class="close" data-dismiss="modal"
+                                                aria-label="Save changes"></button>
+                                        <button type="button" class="close" data-dismiss="modal"
+                                                aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h5>Change icon</h5>
+                                        <i id="smile" class="fas fa-smile" @click="changeActive('smile')"
+                                           v-bind:class="{ active: isActive }"></i>
+                                        <i id="cat" class="fas fa-cat" @click="changeActive('cat')"
+                                           v-bind:class="{ active: isActive }"></i>
+                                        <i id="dragon" class="fas fa-dragon" @click="changeActive('dragon')"
+                                           v-bind:class="{ active: isActive }"></i>
+                                        <i id="hippo" class="fas fa-hippo" @click="changeActive('hippo')"
+                                           v-bind:class="{ active: isActive }"></i>
+                                        <hr>
+                                        <h5>Change color</h5>
+                                        <span class="dot color1"></span>
+                                        <span class="dot color2"></span>
+                                        <span class="dot color3"></span>
+                                        <span class="dot color4"></span>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btnStyle" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btnStyle">Save changes</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         </div>
                     </div>
                 </div>
@@ -45,7 +53,7 @@
                 <button class="btn btnStyle">Contact</button>
             </div>
             <div class="flexSection">
-               <div class="flex" v-for="auction in indivAuctions">
+                <div class="flex" v-for="auction in indivAuctions">
                     <a :href="'/auction/' + auction.auctionid">
                         <p>Auction: {{auction.title}}</p>
                         <p>Ending: {{auction.endtime.toString().replace(/T/g," ")}}</p>
@@ -53,27 +61,25 @@
                 </div>
             </div>
         </div>
-        </div>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'Profile',
         data() {
-            return {}
+            return {
+                isActive: false
+            }
         },
         computed: {
             indivProfile() {
-                console.log(this.$store.state.auctionPosts.filter(auction => auction.users.userid == this.$route.params.userid)[0])
-
                 return this.$store.state.auctionPosts.filter(auction => auction.users.userid == this.$route.params.userid)[0];
             },
             indivAuctions() {
-                console.log(this.$store.state.auctionPosts.filter(auction => auction.users.userid == this.$route.params.userid))
-
                 return this.$store.state.auctionPosts.filter(auction => auction.users.userid == this.$route.params.userid);
             },
-            me(){
+            me() {
                 return this.$store.state.me;
             },
             loggedIn() {
@@ -84,12 +90,15 @@
             showModal() {
                 let element = this.$refs.modal.$el;
                 $(element).modal('show');
+            },
+            changeActive(icon) {
+                document.getElementById(icon).isActive = !this.isActive;
             }
         }
     };
 </script>
 
-<style>
+<style scoped>
     .mainFlex {
         display: flex;
         justify-content: center;
@@ -112,7 +121,9 @@
     }
 
     #profileIcon {
-        height: 50%;
+        height: 30%;
+        width: 30%;
+        align-self: center;
     }
 
     .btnStyle {
@@ -131,17 +142,37 @@
         margin-right: 2em;
     }
 
-    .far, .fas {
+    .fas {
         padding: 0.5em;
         font-size: 2em;
+        outline: #0c5460;
     }
 
-    .color {
+    .dot {
         height: 30px;
         width: 30px;
-        background-color: cornflowerblue;
         border-radius: 50%;
         display: inline-block;
         margin: 1em;
+    }
+
+    .color1 {
+        background-color: cornflowerblue;
+    }
+
+    .color2 {
+        background-color: #e83e8c;
+    }
+
+    .color3 {
+        background-color: limegreen;
+    }
+
+    .color4 {
+        background-color: gold;
+    }
+
+    .active {
+        color: rgb(126, 199, 199);
     }
 </style>

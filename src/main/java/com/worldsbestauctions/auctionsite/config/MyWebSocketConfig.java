d8608a2006c0997089f2ea95ba9fcbce6dc6a7c3
Kristoffer <doedmannen@aetmej.se>
2019-04-25 21:42:35 +0200

    copy pasta from java18 blog, thx dennis

---
+++ src/main/java/com/worldsbestauctions/auctionsite/config/MyWebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.worldsbestauctions.auctionsite.config;
+
+import com.worldsbestauctions.auctionsite.controllers.SocketController;
+import com.worldsbestauctions.auctionsite.services.SocketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class MyWebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    SocketService socketService;
+
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        SocketController sc = new SocketController();
+        sc.setSocketService(socketService);
+        registry.addHandler(sc, "/websocket");
+    }
+}
+

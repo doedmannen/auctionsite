cf7104793cdaf711d4d837c3daeca6a0187f78ea
Kristoffer <doedmannen@aetmej.se>
2019-04-25 09:43:35 +0200

    added order to entity images and auction now sorts images

--- src/main/java/com/worldsbestauctions/auctionsite/entities/Images.java
+++ src/main/java/com/worldsbestauctions/auctionsite/entities/Images.java
@@ -11,11 +11,19 @@ public class Images {
     private long auctionid;
     @Id
     private String path;
-
+    private int order;
     public Images(){}
     public Images(long auctionid, String path){
         this.auctionid = auctionid;
         this.path = path;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
     }
 
     public void setAuctionid(int auctionid) {

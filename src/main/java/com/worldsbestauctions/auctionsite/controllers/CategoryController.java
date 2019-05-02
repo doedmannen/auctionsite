de68ba686ccd4807c7bcb65c56bb69aabcf4109e
Kristoffer <doedmannen@aetmej.se>
2019-05-02 18:03:12 +0200

    categories needed to be sorted

--- src/main/java/com/worldsbestauctions/auctionsite/controllers/CategoryController.java
+++ src/main/java/com/worldsbestauctions/auctionsite/controllers/CategoryController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
@@ -15,7 +19,10 @@ public class CategoryController {
     CategoryService categoryService;
 
     @GetMapping
-    Iterable<Category> getAllCategories(){
-        return categoryService.getAllCategories();
+    List<Category> getAllCategories(){
+        return ((List<Category>) categoryService.getAllCategories())
+                .stream()
+                .sorted(Comparator.comparing(Category::getCategoryname))
+                .collect(Collectors.toList());
     }
 }
